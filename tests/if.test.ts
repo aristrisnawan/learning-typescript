d6d2ec06b0a6e029fe5bf4858ca0b7b76f40b782
aristrisnawan <aristrisnawan35@gmail.com>
2024-07-11 21:18:04 +0700

    day 3 - looping

--- tests/if.test.ts
+++ tests/if.test.ts
@@ -10,4 +10,25 @@ describe("If", () => {
       console.info("Try again");
     }
   });
+
+  it("should support ternary operator", () => {
+    const value = 90;
+    const say = value >= 75 ? "Congratulation" : "Try again";
+
+    console.info(say);
+  });
+  it("should support switch statement", () => {
+    const sayHello = (name: string): string => {
+        switch (name) {
+            case 'Asep':
+                return `Hello Asep`;
+            case 'Budo':
+                return `Hello Budi`;
+            default:
+                return `Hello`;
+        }
+    }
+
+    console.info(sayHello('Asep'));
+  });
 });
